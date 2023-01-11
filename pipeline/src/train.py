c6c7e7c2ad0e975a266fbf48df17aef01b4c9c89
Rizo-R <56843532+Rizo-R@users.noreply.github.com>
2023-01-10 16:50:55 -0800

    Initial commit

---
+++ pipeline/src/train.py
@@ -0,0 +1,81 @@
+
+import argparse
+import os
+import glob
+
+import numpy as np
+import pandas as pd
+from sklearn import ensemble
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+from azure.identity import DefaultAzureCredential 
+from azure.ai.ml.entities import AzureDataLakeGen2Datastore
+from azure.ai.ml import MLClient
+
+import mlflow
+import mlflow.sklearn
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", type=str, help="path to input data")
+    parser.add_argument("--registered_model_name", type=str, help="model name")
+    args = parser.parse_args()
+
+    ml_client = MLClient.from_config(DefaultAzureCredential())
+    ws = ml_client.workspaces.get(ml_client.workspace_name) 
+
+    df = pd.read_csv(args.data, header=1, index_col=0)
+
+    mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
+
+    # Start Logging
+    mlflow.start_run()
+
+    # enable autologging
+    mlflow.sklearn.autolog()
+
+    X = df_new.loc[:, df_new.columns != "label"]
+    y = df_new["label"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    params = {
+        # "n_estimators": 500,
+        # "max_depth": 4,
+        # "min_samples_split": 5,
+        "learning_rate": 0.01,
+        "loss": "squared_error",
+    }
+
+
+    reg = ensemble.HistGradientBoostingRegressor(**params)
+    reg.fit(X_train, y_train)
+
+    mse = mean_squared_error(y_test, reg.predict(X_test))
+    print("The mean squared error (MSE) on test set: {:.4f}".format(mse))
+    mlflow.log_metric("MSE", mse)
+
+    # Registering the model to the workspace
+    print("Registering the model via MLFlow")
+    mlflow.sklearn.log_model(
+        sk_model=reg,
+        registered_model_name=args.registered_model_name,
+        artifact_path=args.registered_model_name
+    )
+
+    # # Saving the model to a file
+    print("Saving the model via MLFlow")
+    mlflow.sklearn.save_model(
+        sk_model=reg,
+        path=os.path.join(registered_model_name, "trained_model"),
+    )
+    ###########################
+    #</save and register model>
+    ###########################
+    mlflow.end_run()
+
+if __name__ == '__main__':
+    main()
