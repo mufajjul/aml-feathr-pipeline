2fb2b6c2d34177cbb3fb953fd52413444a535de6
Rizo-R <56843532+Rizo-R@users.noreply.github.com>
2023-01-19 13:44:21 -0800

    Fix environment variables

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,20 +1,6 @@
 import argparse
 import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--resource_prefix", type=str, help="resource prefix")
-parser.add_argument("--azure_client_id", type=str)
-parser.add_argument("--azure_tenant_id", type=str)
-parser.add_argument("--azure_client_secret", type=str)
-args = parser.parse_args()
-RESOURCE_PREFIX = args.resource_prefix
-os.environ['AZURE_CLIENT_ID'] = args.azure_client_id
-os.environ['AZURE_TENANT_ID'] = args.azure_tenant_id
-os.environ['AZURE_CLIENT_SECRET'] = args.azure_client_secret
-print("AZURE_TENANT_ID:", os.environ['AZURE_TENANT_ID'])
-print("AZURE_TENANT_ID_SUBSTRING", os.environ['AZURE_TENANT_ID'][:2])
-print(os.environ['AZURE_TENANT_ID'] == "72f988bf-86f1-41af-91ab-2d7cd011db47")
-
 from datetime import timedelta
 from math import sqrt
 from pathlib import Path
