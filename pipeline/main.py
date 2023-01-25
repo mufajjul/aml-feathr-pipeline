f328a96fff48879d64c4a43fa123d084c5a42400
Rizo-R <56843532+Rizo-R@users.noreply.github.com>
2023-01-25 09:28:49 -0800

    Small fixes

--- pipeline/main.py
+++ pipeline/main.py
@@ -29,11 +29,13 @@ from feathr import (
     # Offline feature computation
     FeatureQuery, ObservationSettings,
 )
-from feathr.datasets import nyc_taxi
+# from feathr.datasets import nyc_taxi
 from feathr.spark_provider.feathr_configurations import SparkExecutionConfiguration
-from feathr.utils.config import generate_config
+# from feathr.utils.config import generate_config
 from feathr.utils.job_utils import get_result_df
-from feathr.utils.platform import is_databricks, is_jupyter
+# from feathr.utils.platform import is_databricks, is_jupyter
+import nyc_taxi
+from utils import is_databricks, is_jupyter
 
 print(f"Feathr version: {feathr.__version__}")
 
